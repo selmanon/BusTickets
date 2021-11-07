4a97a3c72ef231c15142b4d0abcb4d94fe2fd0f3
aselassi <aselassi@payplug.com>
2021-11-07 12:01:28 +0100

    WIP hilt fix

--- app/src/main/java/com/yavin/mybustickets/di/AppModule.kt
+++ app/src/main/java/com/yavin/mybustickets/di/AppModule.kt
@@ -2,14 +2,17 @@ package com.yavin.mybustickets.di
 
 import android.app.Application
 import android.content.Context
+import androidx.room.Room
 import com.yavin.mybustickets.TicketSoldRoomDatabase
 import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -28,5 +31,20 @@ abstract class AppModule {
         fun provideDispatcher(): CoroutineDispatcher {
             return Dispatchers.IO
         }
+
+        @Singleton // Tell Dagger-Hilt to create a singleton accessible everywhere in ApplicationCompenent (i.e. everywhere in the application)
+        @Provides
+        fun provideYourDatabase(
+            @ApplicationContext app: Context
+        ) = Room.databaseBuilder(
+            app,
+            TicketSoldRoomDatabase::class.java,
+            "TicketSold_database"
+        ).build()
+
+        @Singleton
+        @Provides
+        fun provideYourDao(db: TicketSoldRoomDatabase) = db.ticketSoldDao()
     }
+
 }
